e7d15ee242b48dd20d11a9b0160a3952c513f4e4
Tim Cheng <timcheng.xyz@gmail.com>
2025-07-27 19:39:00 +0800

    test: use new test data

--- main_test.go
+++ main_test.go
@@ -18,10 +18,10 @@ func TestEncrypt(t *testing.T) {
 
 func TestDecrypt(t *testing.T) {
 
-	inputFile, _ := os.Open("./test-data/test-encrypt.out")
+	inputFile, _ := os.Open("./test-data/article.cipher")
 	defer inputFile.Close()
 
-	DecryptFile(inputFile, []byte("GO-OPENSSL-AES-TEST"), 200000, "./test-data/article.cipher")
+	DecryptFile(inputFile, []byte("GO-OPENSSL-AES-TEST"), 200000, "./test-data/test-decrypt.out")
 
 	outputFile, _ := os.Open("./test-data/test-decrypt.out")
 	defer outputFile.Close()
@@ -41,7 +41,7 @@ func TestDecrypt(t *testing.T) {
 	io.Copy(testEncryptedHash, outputFile)
 
 	// sha256 of article.txt
-	const compareHash = "0c74c9a7791623d2f8a743c2283457d23a6e67afcba2381ce1a3a5fd373af01b"
+	const compareHash = "c402eb9f730f961ad00621f1ec5b8bc0608782825f449fd40f61fd5cd60c7875"
 	decryptedHash := hex.EncodeToString(testEncryptedHash.Sum(nil))
 
 	if compareHash != decryptedHash {
@@ -68,7 +68,7 @@ func TestEncryptDecrypt(t *testing.T) {
 	io.Copy(testEncryptedHash, outputFile)
 
 	// sha256 of article.txt
-	const compareHash = "0c74c9a7791623d2f8a743c2283457d23a6e67afcba2381ce1a3a5fd373af01b"
+	const compareHash = "c402eb9f730f961ad00621f1ec5b8bc0608782825f449fd40f61fd5cd60c7875"
 	decryptedHash := hex.EncodeToString(testEncryptedHash.Sum(nil))
 	if compareHash != decryptedHash {
 		t.Fail()
